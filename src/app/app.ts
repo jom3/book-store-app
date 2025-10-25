509d462d7a25163dd690071c71214fe301ee641b
joams mogro gomez <joamsmg@gmail.com>
2025-10-24 23:04:52 -0400

    feat: some files were adde before start

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('bookStoreApp');
+  protected readonly title = signal('BookStore - your friendly store');
 }
